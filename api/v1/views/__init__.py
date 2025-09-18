0d0ec2d5ed71ac145c6b4c15a45debf1589ca6b7
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-18 10:23:11 +0100

    feat(api/v1/views): register course_departments and session_auth routes
    - add course_departments view to blueprint
    - add session_auth view to blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.admins import *
 from api.v1.views.courses import *
+from api.v1.views.course_departments import *
 from api.v1.views.departments import *
 from api.v1.views.feedbacks import *
 from api.v1.views.files import *
@@ -22,3 +23,4 @@ from api.v1.views.notifications import *
 from api.v1.views.reports import *
 from api.v1.views.tutorial_links import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import *
