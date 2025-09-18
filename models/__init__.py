a6b387ee41121af8c8abc4d5e4663f2a3d04c5bc
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-18 10:41:06 +0100

    refactor(models): simplify storage initialization
    - remove support for file storage and env-based storage type selection
    - always use DBStorage as the persistence backend

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+from models.engine.db_storage import DBStorage
 
-if os.getenv("UNIBENENGVAULT_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-
-    storage = DBStorage()
-    storage.reload()
-elif os.getenv("UNIBENENGVAULT_TYPE_STORAGE") == "file":
-    from models.engine.file_storage import FileStorage
-
-    storage = FileStorage()
-    storage.reload()
+storage = DBStorage()
+storage.reload()
