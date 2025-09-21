a8026149f473283db996ea23c05702dffd8c2ef0
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-21 03:53:48 +0100

    feat(models/): configure DBStorage for test environment via .env

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 
+from dotenv import load_dotenv
+import os
+
 from models.engine.db_storage import DBStorage
 
-storage = DBStorage()
-storage.reload()
+
+load_dotenv()
+
+
+if os.getenv("UNIBENENGVAULT_ENV") == "test":
+    db_url = os.getenv("DATABASE_URL_TEST")
+    if not db_url:
+        raise ValueError("No test database found")
+    
+    storage = DBStorage(database_url=db_url)
+    storage.reload()
