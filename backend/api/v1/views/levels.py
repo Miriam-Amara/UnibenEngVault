906af0b9d4535805446a078d9b583ffbacc9dcbd
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-01 03:42:50 +0100

    feat: add get_level_dict(),. Update get_all_levels() to filter by date, search and pagination. Add docstrings.

--- backend/api/v1/views/levels.py
+++ backend/api/v1/views/levels.py
@@ -5,7 +5,7 @@
 """
 
 
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 import logging
 
 from api.v1.views import app_views
@@ -21,10 +21,23 @@ from models.level import Level
 logger = logging.getLogger(__name__)
 
 
+def get_level_dict(level: Level) -> dict[str, int]:
+    """
+    Returns a json serializable dict of the level object.
+    """
+    level_dict = level.to_dict()
+    level_dict["no_of_users_in_level"] = len(level.users)
+    level_dict["no_of_courses_in_level"] = len(level.courses) 
+    level_dict.pop("__class__", None)
+
+    return level_dict
+
+
 @app_views.route("/levels", strict_slashes=False, methods=["POST"])
 @admin_only
 def create_level():
     """
+    Create and add new level to the database.
     """
     valid_data = validate_request_data(LevelCreate)
 
@@ -32,22 +45,37 @@ def create_level():
     db = DatabaseOp()
     db.save(level)
 
-    return jsonify(level.to_dict()), 201
+    level_dict = get_level_dict(level)
+    return jsonify(level_dict), 201
 
 
 @app_views.route(
-        "/levels/<int:page_size>/<int:page_num>",
+        "/levels",
         strict_slashes=False,
         methods=["GET"]
     )
-def get_all_levels(page_size: int, page_num: int):
+def get_all_levels():
     """
+    Returns all levels with optional date filtering, search and pagination.
     """
-    levels = storage.all(Level, page_size, page_num)
-    if not levels:
-        abort(404, description="no level found")
+    page_size: str | None = request.args.get("page_size")
+    page_num: str | None = request.args.get("page_num")
+    created_at: str | None = request.args.get("created_at")
+    search_str: str | None = request.args.get("search_str")
 
-    all_levels = [level.to_dict() for level in levels]
+    if search_str:
+        levels = storage.search(
+            Level, search_str, page_size=page_size, page_num=page_num
+        )
+    else:
+        levels = storage.all(
+            Level, page_size=page_size, page_num=page_num, date_time=created_at
+        )
+
+    if not levels:
+        abort(404, description="No level found.")
+
+    all_levels = [get_level_dict(level) for level in levels]
     return jsonify(all_levels), 200
 
 
@@ -58,12 +86,13 @@ def get_all_levels(page_size: int, page_num: int):
     )
 def get_level(level_id: str):
     """
+    Return a level by its id.
     """
     level = get_obj(Level, level_id)
     if not level:
         abort(404, description="Level does not exist.")
 
-    return jsonify(level.to_dict()), 200
+    return jsonify(get_level_dict(level)), 200
 
 
 @app_views.route(
@@ -73,6 +102,9 @@ def get_level(level_id: str):
     )
 @admin_only
 def delete_level(level_id: str):
+    """
+    Delete a level given the level id.
+    """
     level = get_obj(Level, level_id)
     if not level:
         abort(404, description="Level does not exist.")
