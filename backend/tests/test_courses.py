3f827fa60b52e4adc3739a036a4d3eeaf6824377
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-01 03:56:40 +0100

    feat: add new test methods - test_get_all_courses(), test_gel_all_courses_with_pagination(), and test_search_courses_by_course_codes(). Add docstrings.

--- backend/tests/test_courses.py
+++ backend/tests/test_courses.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-
+Implements unit test cases for courses routes.
 """
 
 
@@ -16,7 +16,7 @@ from models import storage
 from models.course import Course
 from models.level import Level
 from models.user import User
-from tests.requests_data import course_data, level_data
+from tests.requests_data import courses_data, levels_data
 
 
 logger = logging.getLogger(__name__)
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 class TestCourseRoute(unittest.TestCase):
     """
     POST - /api/v1/courses
-    GET - /api/v1/courses/<int:page_size>/<int:page_num>
+    GET - /api/v1/courses
     GET - /api/v1/courses/<course_id>
     UPDATE - /api/v1/courses/<course_id>
     DELETE - /api/v1/courses/<course_id>
@@ -33,6 +33,7 @@ class TestCourseRoute(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         """
+        Creates and login an admin user before execution of the test methods.
         """
         cls.app: Flask = create_app()
         cls.client: FlaskClient = cls.app.test_client()
@@ -60,19 +61,21 @@ class TestCourseRoute(unittest.TestCase):
 
     def add_levels(self) -> None:
         """
+        Create new levels.
         """
-        self.level_data = level_data
+        self.levels = levels_data
         self.level_ids: list[str] = []
 
-        for data in self.level_data:
+        for level in self.levels:
             response = self.client.post(
                 "/api/v1/levels",
-                json=data
+                json=level
             )
             self.level_ids.append(response.get_json().get("id"))
 
     def delete_levels(self) -> None:
         """
+        Delete levels created.
         """
         for level_id in self.level_ids:
             self.client.delete(
@@ -81,30 +84,26 @@ class TestCourseRoute(unittest.TestCase):
 
     def setUp(self) -> None:
         """
+        Create new courses before the execution of each test method.
         """
         self.add_levels()
-        self.course_data = course_data
+        self.courses = courses_data
         self.course_ids: list[str] = []
-        self.responses: list[dict[str, Any]] = []
+        self.add_course_responses: list[dict[str, Any]] = []
 
-        for index, data in enumerate(self.course_data):
-            data["level_id"] = self.level_ids[index]
-            self.response = self.client.post(
+        for index, course in enumerate(self.courses):
+            course["level_id"] = self.level_ids[index]
+            response = self.client.post(
                 f"/api/v1/courses",
-                json=data
+                json=course
             )
-            self.course_ids.append(self.response.get_json().get("id"))
-            self.responses.append(
-                {
-                    "status_code": self.response.status_code,
-                    "response_json": self.response.get_json()
-                }
-            )
+            self.course_ids.append(response.get_json().get("id"))
+            self.add_course_responses.append(response.get_json())
 
     def tearDown(self) -> None:
         """
+        Deletes created courses after each test execution.
         """
-        logger.debug(self.course_ids)
         for course_id in self.course_ids:
             self.client.delete(
                 f"/api/v1/courses/{course_id}"
@@ -119,6 +118,7 @@ class TestCourseRoute(unittest.TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         """
+        Deletes the admin user after executing the class.
         """
         cls.client.delete(
             f"/api/v1/users/{cls.user_id}"
@@ -128,28 +128,66 @@ class TestCourseRoute(unittest.TestCase):
     
     def test_add_courses(self):
         """
+        Test that a course is successfully created.
         """
-        for response in self.responses:
-            self.assertEqual(response["status_code"], 201)
+        for response_json in self.add_course_responses:
+            self.assertIn("course_code", response_json)
+    
+    def test_get_all_courses(self):
+        """
+        Test that all courses in the database are returned.
+        """
+        response = self.client.get("/api/v1/courses")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.get_json()), len(self.courses))
+    
+    def test_get_all_courses_with_pagination(self):
+        """
+        Test that courses retrieval are limited by the page size given.
+        """
+        response = self.client.get(
+            "/api/v1/courses",
+            query_string={"page_size": 3, "page_num": 1}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.get_json()), 3)
+    
+    def test_search_courses_by_course_codes(self):
+        """
+        Test that only the courses that match the search string are retrieved.
+        """
+        response = self.client.get(
+            "/api/v1/courses",
+            query_string={"search_str": "ide"}
+        )
+        self.assertEqual(response.status_code, 200)
 
-    def test_get_courses(self):
+    def test_get_course(self):
         """
+        Test that a course is retrieved successfully.
+        Verify __class__ is not in the response.
         """
         response = self.client.get(f"/api/v1/courses/{self.course_ids[0]}")
+
+        course_data = response.get_json()
         self.assertEqual(response.status_code, 200)
-        self.assertIn("course_code", response.get_json())
-        self.assertIn("semester", response.get_json())
-        self.assertIn("credit_load", response.get_json())
-        self.assertIn("title", response.get_json())
-        self.assertIn("outline", response.get_json())
-        self.assertIn("is_active", response.get_json())
-        self.assertIn("level_id", response.get_json())
-        self.assertIn("admin_id", response.get_json())
-        self.assertIn("files", response.get_json())
-        self.assertIn("departments", response.get_json())
+        self.assertIn("course_code", course_data)
+        self.assertIn("semester", course_data)
+        self.assertIn("credit_load", course_data)
+        self.assertIn("title", course_data)
+        self.assertIn("outline", course_data)
+        self.assertIn("is_active", course_data)
+        self.assertIn("level_id", course_data)
+        self.assertIn("admin_id", course_data)
+        self.assertIn("course_level", course_data)
+        self.assertIn("num_of_files_in_course", course_data)
+        self.assertIn("course_departments", course_data)
+        self.assertIn("added_by", course_data)
+        self.assertNotIn("__class__", course_data)
 
     def test_update_course(self):
         """
+        Test that a course is updated successfully.
         """
         course = self.client.get(f"/api/v1/courses/{self.course_ids[0]}")
         self.assertEqual(course.get_json().get("semester"), "first")
