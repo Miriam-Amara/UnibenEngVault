d5953b417dee9ad30e8b3c464ebedf78b2467436
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-02 17:36:38 +0100

    feat: add get_course_dict() method. Implement db get_courses_by_dept_and_level(). Add docstrings.

--- backend/api/v1/views/course_departments.py
+++ backend/api/v1/views/course_departments.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 """
-
+Implements routes for CRUD (Create, Read, Update and Delete)
+operations on combination of courses and departments.
 """
 
 
-from flask import request, abort, jsonify
+from flask import abort, jsonify, request
 from typing import Any
 import logging
 
 from api.v1.views import app_views
 from api.v1.auth.authorization import admin_only
 from api.v1.utils.utility import get_obj, DatabaseOp
+from models import storage
 from models.course import Course
 from models.department import Department
 from models.level import Level
@@ -20,73 +22,33 @@ from models.level import Level
 logger = logging.getLogger(__name__)
 
 
-def get_course_departments_dict(course: Course) -> dict[str, list[str]]:
+def get_course_dict(course: Course) -> dict[str, Any]:
     """
+    Returns a json serializable dict of the course object.
     """
-    course_departments = [
-        department.dept_code for department in course.departments
+    course_dict = course.to_dict()
+    course_dict["course_code"] = course_dict["course_code"].upper()
+    course_dict["level"] = course.level.level_name
+    course_dict["num_of_files_in_course"] = len(course.files)
+    course_dict["departments"] = [
+        department.dept_name for department in course.departments
     ]
-    return {
-        "course_code": course.course_code,
-        "level": course.level.name,
-        "departments": course_departments
-    }
+    course_dict["added_by"] = course.added_by.user.email
+    course_dict.pop("files", None)
+    course_dict.pop("__class__", None)
+
+    return course_dict
 
 
-def get_department_courses_dict(
-        department: Department
-    ) -> dict[str, list[str]]:
-    """
-    """
-    dept_courses = [
-        course.course_code for course in department.courses
-    ]
-    return {
-        "department_name": department.dept_name,
-        "department_code": department.dept_code,
-        "department_courses": dept_courses,
-    }
-
-
-def get_department_level_courses_dict(
-    department: Department,
-    level: Level,
-    semester: str | None = None,
-) -> list[dict[str, Any]]:
-    """
-    Return all courses for a given department and level.
-    If semester is provided, filter courses by that semester.
-    """
-    dept_level_courses: list[dict[str, Any]] = []
-
-    for course in department.courses:
-        if course.level != level:
-            continue
-
-        if semester and course.semester.value != semester:
-            continue
-
-        course_dict = course.to_dict()
-        course_dict.pop("__class__", None)
-        course_dict["course_code"] = course_dict["course_code"].upper()
-        course_dict["level"] = course.level.name
-        course_dict["files"] = len(course.files)
-        course_dict["departments"] = [
-            dept.dept_code.upper() for dept in course.departments
-        ]
-
-        dept_level_courses.append(course_dict)
-    return dept_level_courses
-
-
-# allow only admins
 @app_views.route(
         "/courses/<course_id>/departments/<department_id>",
+        strict_slashes=False,
         methods=["POST"]
     )
 @admin_only
-def add_course_departments(course_id: str, department_id: str):
+def add_department_to_course(course_id: str, department_id: str):
     """
+    Adds a department to a course.
     """
     course = get_obj(Course, course_id)
     if not course:
@@ -101,18 +63,19 @@ def add_course_departments(course_id: str, department_id: str):
         db = DatabaseOp()
         db.save(course)
 
-    course_dict = get_course_departments_dict(course)
+    course_dict = get_course_dict(course)
     return jsonify(course_dict), 201
 
 
-# allow only admins
 @app_views.route(
         "/departments/<department_id>/courses/<course_id>",
+        strict_slashes=False,
         methods=["POST"]
     )
 @admin_only
-def add_department_courses(department_id: str, course_id: str):
+def add_course_to_department(department_id: str, course_id: str):
     """
+    Adds a course to a department
     """
     department = get_obj(Department, department_id)
     if not department:
@@ -127,36 +90,20 @@ def add_department_courses(department_id: str, course_id: str):
         db = DatabaseOp()
         db.save(department)
 
-    dept_dict = get_department_courses_dict(department)
+    dept_dict = get_course_dict(course)
     return jsonify(dept_dict), 201
-
-
-@app_views.route(
-        "/courses/<course_id>/departments",
-        methods=["GET"]
-    )
-def get_course_departments(course_id: str):
-    """
-    """
-    course = get_obj(Course, course_id)
-    if not course:
-        abort(404, description="course does not exist")
-    
-    course_departments = get_course_departments_dict(course)
-    return jsonify(course_departments), 200
-
 
 @app_views.route(
         "/departments/<department_id>/levels/<level_id>/courses",
+        strict_slashes=False,
         methods=["GET"]
     )
-def get_department_level_courses(department_id: str, level_id: str):
+def get_courses_by_department_and_level(department_id: str, level_id: str):
     """
+    Retrieves all courses offered by a specific department and level and
+    optionally filter by semester.
     """
-    semester = request.args.get("semester")
-
-    if semester and semester.lower().strip() not in ["first", "second"]:
-        semester = None
+    semester: str | None = request.args.get("semester")
     
     department = get_obj(Department, department_id)
     if not department:
@@ -166,26 +113,34 @@ def get_department_level_courses(department_id: str, level_id: str):
     if not level:
         abort(404, description="Level does not exist.")
     
-    department_level_courses = get_department_level_courses_dict(
-        department, level, semester=semester
+    courses = storage.get_courses_by_dept_and_level(
+        department.id,
+        level.id,
+        semester=semester
     )
 
-    if not department_level_courses:
+    if not courses:
         abort(
             404,
-            description="No courses found for the department and level"
+            description="No course found for the department and level."
         )
-    return jsonify(department_level_courses), 200
+
+    courses_dict: list[dict[str, Any]] = [
+        get_course_dict(course) for course in courses
+    ]    
+
+    return jsonify(courses_dict), 200
 
 
-# allow only admins
 @app_views.route(
         "/courses/<course_id>/departments/<department_id>",
+        strict_slashes=False,
         methods=["DELETE"]
     )
 @admin_only
-def delete_course_department(course_id: str, department_id: str):
+def delete_department_from_course(course_id: str, department_id: str):
     """
+    Removes a department from a course.
     """
     course = get_obj(Course, course_id)
     if not course:
@@ -198,17 +153,20 @@ def delete_course_department(course_id: str, department_id: str):
     course.departments.remove(department)
     db = DatabaseOp()
     db.save(course)
-    return jsonify({}), 200
+
+    course_dict = get_course_dict(course)
+    return jsonify(course_dict), 200
 
 
-# allow only admins
 @app_views.route(
         "/departments/<department_id>/courses/<course_id>",
+        strict_slashes=False,
         methods=["DELETE"]
     )
 @admin_only
-def delete_department_course(department_id: str, course_id: str):
+def delete_course_from_department(department_id: str, course_id: str):
     """
+    Removes a course from a department
     """
     department = get_obj(Department, department_id)
     if not department:
@@ -221,4 +179,6 @@ def delete_department_course(department_id: str, course_id: str):
     department.courses.remove(course)
     db = DatabaseOp()
     db.save(course)
-    return jsonify({}), 200
+
+    course_dict = get_course_dict(course)
+    return jsonify(course_dict), 200
