ecf828b28013f971538336c724fafe1bd26a1cf0
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-03 04:13:08 +0100

    feat: add courses api requests.

---
+++ frontend/src/api/courses.js
@@ -0,0 +1,83 @@
+/* */
+
+import api from "./api";
+import { showToast } from "../utility/toast";
+
+
+export async function  addCourseApi({course_data}) {
+  try {
+    const response = await api.post("/courses", course_data);
+    return response?.data
+  }
+  catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error adding courses.", "error");
+    console.error("Error adding courses: ", error);
+    throw error;
+  }
+}
+
+
+export async function updateCourseApi({course_id, course_data}) {
+  try {
+    const response = await api.put(`/courses/${course_id}`, course_data);
+    return response?.data
+  }
+  catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error updating courses.", "error");
+    console.error("Error updating courses: ", error);
+    throw error;
+  }
+}
+
+
+export async function fetchAllCoursesApi({page_size, page_num, created_at, search_course_code}) {
+  try {
+    const response = await api.get(
+      "/courses",
+      {params:
+        {
+          page_size: page_size || null,
+          page_num: page_num || null,
+          date_time: created_at || null,
+          search: search_course_code || null,
+        },
+      }
+    );
+    return response?.data
+  }
+  catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error fetching all courses.", "error");
+    console.error("Error fetching all courses: ", error);
+    throw error;
+  }
+}
+
+
+export async function fetchCourseApi({course_id}) {
+  try {
+    const response = await api.get(`/courses/${course_id}`);
+    return response?.data
+  }
+  catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error fetching course.", "error");
+    console.error("Error fetching course: ", error);
+    throw error;
+  }
+}
+
+
+export async function deleteCourseApi({course_id}) {
+  try {
+    await api.delete(`/courses/${course_id}`);
+  }
+  catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error deleting course.", "error");
+    console.error("Error deleting course: ", error);
+    throw error;
+  }
+}
