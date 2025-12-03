6ea3ed5bb96b52862c3c407375df0764bd96318b
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-03 04:21:18 +0100

    feat: add global axios client with auth redirect on 401.

---
+++ frontend/src/api/api.js
@@ -0,0 +1,25 @@
+/*
+Implement a global axios for sending backend requests.
+Redirects user to login if session expires.
+*/
+
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: apiUrl + "/api/v1",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
