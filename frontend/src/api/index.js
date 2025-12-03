3e9e9f1c6334735e5028b2167bddc1078698f76b
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-03 04:15:25 +0100

    feat: add index api requests.

---
+++ frontend/src/api/index.js
@@ -0,0 +1,17 @@
+/* */
+
+import api from "./api";
+import { showToast } from "../utility/toast";
+
+
+export async function fetchStatsApi() {
+  try {
+    const response = api.get("/stats");
+    return response?.data;
+  } catch (error) {
+    if (error.message)
+      showToast(error?.response?.data?.error || "Error fetching stats.", "error");
+    console.error("Error fetching stats: ", error);
+    throw error;
+  }
+}
