322c29fe3434a20dcf91fe8e0c1919ffc399b1b9
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-04 06:40:03 +0100

    feat: import main.css. Remove env variable import.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
-import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_APP_URL
-axios.defaults.withCredentials = true
+import App from './App.jsx';
+import './index.css';
+import './styles/main.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
