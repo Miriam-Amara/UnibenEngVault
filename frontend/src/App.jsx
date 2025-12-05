4cc8008265182a9815623106db53430d25d84b9f
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-05 17:16:14 +0100

    feat: add register and login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/General/HomePage";
+import ResgisterPage from "./pages/auth/ResgisterPage";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
   return(
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<ResgisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
