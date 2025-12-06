5349c9f8352f47da18ebcadcf147de49f83a92c9
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-06 20:09:39 +0100

    feat: add Header component

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+
+import { logoutApi } from "../../api/users";
+import { useAuth } from "../../pages/auth/AuthContext";
+import { Button } from "../ui/Button";
+import sidebarIcon from "../../assets/sidebar_menu_icon.png";
+
+
+
+export default function Header({
+  showSidebar,
+  setShowSidebar,
+  className
+}) {
+
+  const { user, logout } = useAuth();
+
+  console.log("user info: ", user );
+  const handleLogout = async () => {
+    try {
+      await logoutApi();
+      logout();
+
+    } catch (error) {
+      console.error("Error in logout: ", error)
+    }
+  };
+
+
+  return(
+    <header
+      className={`h-10 w-screen py-1 px-5 bg-primary flex items-center ${className}`}
+    >
+      <img
+        src={ sidebarIcon }
+        alt="sidebar"
+        onClick={ () => { setShowSidebar(!showSidebar); } }
+        className="h-75 mr-4 py-1 px-3 rounded-5 hover-bg-grey-25 cursor-pointer"
+      />
+      <h4 className="text-primary-dark">UnibenEngVault</h4>
+      <ul>
+        <li>{ user?.dept_code }</li>
+        <li>{ user?.level_name}</li>
+      </ul>
+
+      <Button
+        type="button"
+        variant="secondary"
+        size="md"
+        onClick={ handleLogout }
+        children="Logout"
+        className="ml-auto"
+      />
+    </header>
+  );
+}
