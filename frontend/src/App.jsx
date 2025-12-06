5e6f76b60d50351a05eb08d166d32069edc6c7b3
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-06 20:13:21 +0100

    feat: add profile page. Add admin route and AuthProvider for route authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
+import { AdminRoute, AuthProvider } from "./pages/auth/AuthContext";
 import HomePage from "./pages/General/HomePage";
 import ResgisterPage from "./pages/auth/ResgisterPage";
 import LoginPage from "./pages/auth/LoginPage";
+import ProfilePage from "./pages/General/ProfilePage";
 
 function App() {
   return(
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<ResgisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>  
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<ResgisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
