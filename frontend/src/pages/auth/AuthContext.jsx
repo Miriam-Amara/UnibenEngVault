628e6647fc247dd94207f6ec404dd859426055d9
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-07 21:53:32 +0100

    feat: add fetchMe to fetch user on page reload. add loading to AdminRoute

--- frontend/src/pages/auth/AuthContext.jsx
+++ frontend/src/pages/auth/AuthContext.jsx
@@ -4,21 +4,32 @@
 
 
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+
+import { fetchUserApi } from "../../api/users";
 
 
 const AuthContext = createContext();
 
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [ user, setUser ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
 
   const login = (userData) => setUser(userData);
   const logout = () => setUser(null);
 
+  const fetchMe = async () => {
+    const userData = await fetchUserApi("me");
+    setUser(userData ?? {})
+    setLoading(false);
+  }
+
+  useEffect(() => {fetchMe()},[]);
+
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -29,13 +40,19 @@ export const useAuth = () => useContext(AuthContext);
 
 
 export function AdminRoute({ children }) {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
+  if (loading) {
+    return(
+      <p>Loading...</p>
+    );
+  }
+  
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  if (user.role !== "admin") {
+  if (!user.is_admin) {
     return <Navigate to="/unauthorized" replace />;
   }
 
