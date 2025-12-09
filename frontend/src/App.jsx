ed54aa28199a175fe3f864c44601f07af3a62c4a
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-12-09 12:12:18 +0100

    feat: add levels and users routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import ResgisterPage from "./pages/auth/ResgisterPage";
 import LoginPage from "./pages/auth/LoginPage";
 import ProfilePage from "./pages/General/ProfilePage";
 import DepartmentPage from "./pages/admin/DepartmentsPage";
+import LevelPage from "./pages/admin/LevelsPage";
+import UserPage from "./pages/admin/Users/UsersPage";
 
 function App() {
   return(
@@ -16,11 +18,26 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
 
+        {/* --------------------- ADMIN ROUTES --------------------- */}
         <Route path="/departments" element={
+
           <AdminRoute>
             <DepartmentPage />
           </AdminRoute>}
         />
+
+        <Route path="/levels" element={
+          <AdminRoute>
+            <LevelPage />
+          </AdminRoute>}
+        />
+
+        <Route path="/users" element={
+          <AdminRoute>
+            <UserPage />
+          </AdminRoute>}
+        />
+
       </Routes>
     </AuthProvider>
   );
